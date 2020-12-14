890ff535a2378138b54d646bca9bd641b313cd00
Molly <mollymccollumwx@gmail.com>
2020-12-14 15:18:00 -0500

    fixed DOM manipulation to appear in HTML containers. Created a function to check for correct answers and to increment/decrement score and time. Created a function to end the game if there are no more questions left in the object of arrays.

--- quiz.js
+++ quiz.js
@@ -1,99 +1,159 @@
-console.log("hello World");
-
 //HTML Elements
 var homepage = document.getElementById("homepage");
 var startButton = document.getElementById("start-button");
-var timer = document.getElementById("timer");
-var question = document.getElementById("question");
-var choices = document.getElementById("answer-choices");
-var quizBody = document.getElementById("quiz-content");
+var timerEl = document.getElementById("timer");
+var questionEl = document.getElementById("question");
+var choicesEl = document.getElementById("answer-choices");
+var quizContent = document.getElementById("quiz-content");
+var gameOver = document.getElementById("game-over");
+var endMessage = document.getElementById("end-message");
+var finalScore = document.getElementById("final-score");
 
 
 //Array of Objects containing 5 questions and answer choices
 var quizQuestions = [
   {
-    question: "Commonly used data types DO NOT include:",
+    title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
-    correctAnswer: "alerts",
+    answer: "alerts",
   },
 
   {
-    question: "The condition in an if/else statement is enclosed within _____.",
-    choice1: "quotes",
-    choice2: "curly brackets",
-    choice3: "parentheses",
-    choice4: "square brackets",
+    title: "The condition in an if/else statement is enclosed within _____.",
+    choices: ["quotes", "curly brackets","parentheses", "square brackets"],
+    answer: "curly brackets"
   },
 
   {
-    question: "Arrays in JavaScript can be used to store ____.",
-    choice1: "numbers and strings",
-    choice2: "other arrays",
-    choice3: "booleans",
-    choice4: "all of the above",
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+    
   },
 
   {
-    question:
-      "String values must be enclosed within _____ when being assigned to variables",
-    choice1: "commas",
-    choice2: "curly brackets",
-    choice3: "quotes",
-    choice4: "parentheses",
+    title:"String values must be enclosed within _____ when being assigned to variables",
+    choices: ["commas", "curly brackets", "quotes","parentheses"],
+    answer: "quotes"
   },
 
   {
-    questions:
-      "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choice1: "JavaScript",
-    choice2: "terminal/bash",
-    choice3: "for loops",
-    choice4: "console log",
+    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal/bash", "for loops", "console log"],
+    answer: "console log"
   },
 ];
+//sets starting index of QuizQuestions Object
+var index = 0;
+//time when game starts
+var timeLeft = 75;
+//starting score
+var score = 0;
 
-var quizQuestionsIndex = 0;
 
-console.log(quizQuestions);
+//waits for user to click to start game
+startButton.addEventListener("click", startQuiz);
 
 //starts the quiz after the button is clicked
 function startQuiz() {
   //clears jumbotron from page
-  homepage.style.display = "none";
-  //timer element starts counting down
+  homepage.classList.add("d-none");
+  //displays container for quiz questions
+  quizContent.classList.remove("d-none");
+  
+  //starts the timer
+  timer ();
 
+  //calls function to generate questions
   askQuestions();
 }
 
+function timer (){
+  var timerInterval = setInterval(function(){
+    timeLeft--;
+    timerEl.textContent = timeLeft;
+
+    if (timeLeft === 0){
+      clearInterval(timerInterval);
+      endGame();
+    }
+  }, 1000);
+  
+}
+//generates questions on the page
 function askQuestions() {
-  question.textContent = quizQuestions[quizQuestionsIndex].question;
-  document.body.append(question);
+  //clear out previous question/choices
+  questionEl.textContent = "";
+  
+  //renders the question on the page
+  var question = quizQuestions[index].title;
+  questionEl.append(question);
 
-   var choices = document.createElement("ul");
+  var ulEl = document.createElement("ul");
+  
+  ulEl.addEventListener("click", checkAnswer)
 
-  for (var i = 0; i < 4; i++) {
+  //creates a list of buttons with answer choices
+  for (var i = 0; i < 4; i++){
     var listEl = document.createElement("li");
     var buttonEl = document.createElement("button");
-    buttonEl.setAttribute("data-index", i);
-    buttonEl.textContent = (i + 1) + ". " + quizQuestions[quizQuestionsIndex].choices[i];
+    buttonEl.className = "btn btn-primary btn-lg";
+    buttonEl.textContent = quizQuestions[index].choices[i];
 
+    //appends the button to the list
     listEl.appendChild(buttonEl);
-    choices.appendChild(listEl);
-
-    console.log(buttonEl);
-    console.log(listEl);
-
+    ulEl.appendChild(listEl);
   }
-
-  document.body.append(choices);
+  //adds question list to the HTML
+  questionEl.append(ulEl);
   
+}
+
+//when the user selects an answer choice button
+function checkAnswer(event) {
+  if (event.target.matches("button")){
+    //if the user selects the correct answer button
+    if (event.target.textContent === quizQuestions[index].answer){
+      //adds 10 to the score
+      score += 10;
+      
+    }
+    //if the user selects the wrong answer button
+    else {
+      //deducts 10 points from the score if the score is above 0
+      if (score > 0){
+        score -= 10;
+      }
+      //deducts 10 seconds from the time
+      timeLeft = timeLeft - 10;
+    }
+  }
+  index++;
+
+  if (index > 4 || timeLeft === 0){
+    endGame();
+  } else {
+  askQuestions();
+  }
+}
+
+function endGame(){
+    quizContent.classList.add("d-none");
+    gameOver.classList.remove("d-none");
+
+     
+
+    endMessage.textContent = "Game Over!";
+    finalScore.textContent = "Your final score is: " + score + ".";
+
+    
 
 
 
-  console.log(question);
+
 
   
 }
 
-//waits for user to click start game
-startButton.addEventListener("click", startQuiz);
+
+
